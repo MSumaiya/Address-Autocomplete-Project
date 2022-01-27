cc0c5a7127d6e1704f77a3bd33bc2f5d41a21f60
MSumaiya <mirza01.alam@gmail.com>
2022-01-27 12:37:40 +0100

    Integrated Google Map with Address autocomplete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from "react-places-autocomplete";
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import "./App.css";
 
 function App() {
   const [address, setAddress] = useState("");
   const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
+
 
   const handleSelect = async (value) => {
     const results = await geocodeByAddress(value);
@@ -15,7 +17,32 @@ function App() {
     console.log(ll);
     setAddress(value);
     setCoordinates(ll);
+   
   };
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDR3E0Ko3uYrfQaqX9964woDyJ8NoDn4tg"
+  })
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+
+  /* const center = {
+    lat: 23.6850,
+    lng: 90.3563
+  }; */
 
   return (
     <div>
@@ -60,6 +87,30 @@ function App() {
           </div>
         )}
       </PlacesAutocomplete>
+      
+{
+      isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={{ 
+                lat:coordinates.lat,
+                lng: coordinates.lng
+              }}
+              zoom={30}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+            >
+              { /* Child components, such as markers, info windows, etc. */ }
+              <></>
+              <Marker 
+               position = {{ 
+                lat:coordinates.lat,
+                lng: coordinates.lng
+              }}
+              />
+            </GoogleMap>
+        ) : <></>
+      }
     </div>
   );
 }
